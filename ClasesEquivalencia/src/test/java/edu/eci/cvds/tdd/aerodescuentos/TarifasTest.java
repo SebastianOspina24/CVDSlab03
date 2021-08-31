287ee8630104ae82623f31659087bc263444d8ef
DaeL11 <60555590+DaeL11@users.noreply.github.com>
2021-08-30 22:02:32 -0500

    Realizamos 6 pruebas que sirven de acuerdo al descuento recibido

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -1,15 +1,46 @@
 package edu.eci.cvds.tdd.aerodescuentos;
 
 import org.junit.Assert;
+
 import org.junit.Test;
 
 public class TarifasTest {
 
     @Test
-    public void validateRegistryResult() {
+    public void validateSieteUnoCaso() {
         
-        double resulatado = edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos.calculoTarifa(10,21,66);
-        System.out.println(resulatado+"--------------------------------------------------------------");
+        double result = edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos.calculoTarifa(10,20,13);
+        Assert.assertEquals(result, 10,0);
+    }
+    @Test
+    public void validateSieteDosCaso() {
+        
+        double result = edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos.calculoTarifa(10,20,24);
+        Assert.assertEquals(result, 10,0);
+    }
+    @Test
+    public void validateSieteTresCaso() {
+        
+        double result = edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos.calculoTarifa(10,21,24);
+        Assert.assertEquals(result, 8,5);
+    }
+    @Test
+    public void validateSieteCuatroCaso() {
+        
+        double result = edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos.calculoTarifa(10,24,13);
+        Assert.assertEquals(result, 8,0);
+    }
+    @Test
+    public void validateSieteCincoCaso() {
+        
+        double result = edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos.calculoTarifa(10,24,66);
+        Assert.assertEquals(result, 7,7);
+    }
+    @Test
+    public void validateSieteSeisCaso() {
+        
+        double result = edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos.calculoTarifa(10,13,66);
+        Assert.assertEquals(result, 9,2);
     }
 
 }
