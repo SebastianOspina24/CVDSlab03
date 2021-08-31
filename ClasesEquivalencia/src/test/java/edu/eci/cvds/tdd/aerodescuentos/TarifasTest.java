dc1a4036d99990bd9960206a7cd63630db026a52
juan Sebastian Ospina <juansebastian200103@gmail.com>
2021-08-30 21:47:57 -0500

    Adicionamos aerodescuentos al pom y se crea la primera prueba

--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/aerodescuentos/TarifasTest.java
@@ -1,5 +1,15 @@
 package edu.eci.cvds.tdd.aerodescuentos;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TarifasTest {
-    
+
+    @Test
+    public void validateRegistryResult() {
+        
+        double resulatado = edu.eci.cvds.tdd.aerodescuentos.CalculadorDescuentos.calculoTarifa(10,21,66);
+        System.out.println(resulatado+"--------------------------------------------------------------");
+    }
+
 }
